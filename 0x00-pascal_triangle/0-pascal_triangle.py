55663a6550c3ffc9eb83b8c03142a43c40277fb0
Initech <inimfonebong001@gmail.com>
2024-01-04 14:31:35 +0000

    creat a pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+"""module for def pascal_triangle(n): """
+
+def pascal_triangle(n):
+    """ create a pascal triangl of size n """
+    triangle = []
+    if n <= 0:
+        return triangle
+    for i in range(n):
+        triangle.append([1] * (i + 1))
+
+    for row in range(2, n):
+        for col in range(1, row):
+            triangle[row][col] = triangle[row - 1][col - 1] + triangle[row - 1][col]
+
+    return triangle
