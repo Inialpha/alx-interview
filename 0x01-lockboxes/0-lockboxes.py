6a692d92fed7533b858489a31d6b8c55887e5c56
Initech <inimfonebong001@gmail.com>
2024-01-10 15:36:27 +0000

    Add task 0

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" module contain canUnlockAll(boxes) function """
+
+
+def canUnlockAll(boxes):
+    """ function determines if all boxes can opened """
+    myKeys = boxes[0]
+    opened = [0]
+    openedboxes = [boxes[0]]
+
+    for mykey in myKeys:
+        if mykey not in opened:
+            myKeys.extend(boxes[mykey])
+            opened.append(mykey)
+            openedboxes.append(boxes[mykey])
+
+    return len(openedboxes) == len(boxes)
