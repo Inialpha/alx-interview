491f64de5269b3e171b6a29d209d386ecec48744
Initech <inimfonebong001@gmail.com>
2024-01-25 11:24:44 +0000

    Add regex

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
 """A script that reads stdin line by line and computes metrics
 """
+import re
 from sys import stdin, stdout
 try:
     num = 0
     status_dict = {}
     total_size = 0
     for line in stdin:
-        num += 1
-        line = line.split()
-        ip = line[0]
-        status = line[-2]
-        file_size = line[-1]
-        total_size += int(file_size)
-        if status in status_dict:
-            status_dict[status] += 1
-        else:
-            status_dict[status] = 1
-        if num % 10 == 0:
-            print(f"File size: {total_size}")
-            for key, value in status_dict.items():
-                print(f"{key}: {value}")
+        pattern = re.compile(r'^(\d+\.\d+\.\d+\.\d+) - \[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6})\] "(GET|POST|PUT|DELETE) (/.+?) HTTP/\d\.\d" (\d+) (\d+)$')
+
+        line = pattern.match(line)
+        if line:
+            num += 1
+            ip = line.group(1)
+            status = line.group(5)
+            file_size = line.group(6)
+            total_size += int(file_size)
+            if status in status_dict:
+                status_dict[status] += 1
+            else:
+                status_dict[status] = 1
+            if num % 10 == 0:
+                print(f"File size: {total_size}")
+                for key, value in status_dict.items():
+                    print(f"{key}: {value}")
 except (KeyboardInterrupt, EOFError):
     print(f"File size: {total_size}")
     for key, value in status_dict.items():
