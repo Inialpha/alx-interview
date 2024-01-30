8740fc89751972a1d5fd6d3482c26c1d831623e9
Initech <inimfonebong001@gmail.com>
2024-01-30 17:40:23 +0000

    Add task 0

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" module for validUTF8 function """
+
+
+def validUTF8(data):
+    """ Return: True if data is a valid UTF-8 encoding, else return False
+    """
+    try:
+        bytes(data).decode('utf-8')
+        return True
+    except (UnicodeDecodeError, ValueError):
+        return False
