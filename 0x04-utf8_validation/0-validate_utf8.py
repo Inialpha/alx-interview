893e3760bbfcc4e5da499f087a45bfddc9746de5
Initech <inimfonebong001@gmail.com>
2024-01-30 18:02:28 +0000

    handle value error

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,5 +8,5 @@ def validUTF8(data):
     try:
         bytes(data).decode('utf-8')
         return True
-    except (UnicodeDecodeError, ValueError):
+    except (UnicodeDecodeError, ValueError, TypeError):
         return False
