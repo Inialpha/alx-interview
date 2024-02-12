9a80978cfb6289f54381cbcd96c20f1d58f92378
Inialpha <inimfonebong001@gmail.com>
2024-02-12 09:55:40 -0800

    tested

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,38 +1,41 @@
 #!/usr/bin/node
 const request = require('request');
-const argv = require(' process.argv')
 
-if (argv.length == 3) {
-	const id = argv[2]
-}
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`
+const id = process.argv[2];
 
-request(url, function(error, response, body) {
-    if (error)
-	// pass
-    film = JSON.parse(body)
-    characters = film['characters']
-});
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-function get_req (url) {
-    const res = new Promise(function(resolve, reject) {
-	request(url, function(error, response, body) {
-	    if (response === 200) {
-		person = JSON.parse(body);
-		resolve(person['name']);
-	    } else {
-		reject(respose)
-	    }
-	});
+function getReq (url) {
+  const res = new Promise(function (resolve, reject) {
+    request(url, function (error, response, body) {
+      if (error) {
+        // pass
+      }
+
+      if (response.statusCode === 200) {
+        const person = JSON.parse(body);
+        resolve(person.name);
+      } else {
+        reject(response);
+      }
     });
-    return res;
+  });
+  return res;
 }
 
-for (character of characters) {
-    get_req(url).then((result) => (console.log(result)))
-    .catch((error) => (console.log(error)))
-}
+request(url, function (error, response, body) {
+  if (error) {
+    // pass
+  }
+  const film = JSON.parse(body);
+  const characters = film.characters;
 
-
-
-
+  const promises = characters.map(character => getReq(character));
+  Promise.all(promises)
+    .then(results => {
+      results.forEach(result => console.log(result));
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
